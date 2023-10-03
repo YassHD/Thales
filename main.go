bd888a8fb5774f52fb6cf695c5b1b4c569cb0db1
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2023-10-03 14:03:00 +0200

    v gin mais c pas ça

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import {
-	gin
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func openBase() {
+	//Ouvre la connexion à la base de données
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/forum")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-router.Run("localhost:8080")
+func main() {
+	router := gin.Default()
+	// Démarre le serveur
+	openBase()
+	// Ouvre le Database
+
+	router.Static("/static", "./static") // charge le css et le script js
+	router.LoadHTMLGlob("tmpl/*.html")   // charge les pages HTML
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "inscription.html", gin.H{})
+	})
+
+}
